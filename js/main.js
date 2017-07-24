6e268b8c8410ab155217f64a03fc364659ab3872
elijah-breck <elijah@widehorizonmedia.com>
2017-07-24 14:59:11 -0400

    js functions for cards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
-$(document).ready(function() {
+var MatchGame = {};
 
-  $('p').hover(function() {
-    $(this).toggleClass('highlight');
-  });
-});
+/*
+  Sets up a new game after HTML document has loaded.
+  Renders a 4x4 board of cards.
+*/
+
+/*
+  Generates and returns an array of matching card values.
+ */
+
+MatchGame.generateCardValues = function () {
+  var cardValues = [];
+  for (var i = 1; i <= 8; i++) {
+    cardValues.push(i);
+    console.log("Hello world, " + cardValues);
+  }
+};
+
+/*
+  Converts card values to jQuery card objects and adds them to the supplied game
+  object.
+*/
+
+MatchGame.renderCards = function(cardValues, $game) {
+
+};
+
+/*
+  Flips over a given card and checks to see if two cards are flipped over.
+  Updates styles on flipped cards depending whether they are a match or not.
+ */
+
+MatchGame.flipCard = function($card, $game) {
+
+};
+
+MatchGame.generateCardValues();
